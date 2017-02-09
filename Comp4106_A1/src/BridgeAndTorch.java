12e756e03accf9390b2a01c71afa20c1e39d064f
marcteboekhorst <marcteboekhorst@5010-018.ODENG2.CARLETON.CA>
2017-02-09 15:53:30 -0500

    commit 1

--- Comp4106_A1/src/BridgeAndTorch.java
+++ Comp4106_A1/src/BridgeAndTorch.java
@@ -3,21 +3,70 @@ import java.util.ArrayList;
 
 public class BridgeAndTorch {
 
-	private int[] people;
+	private static int[] people;
 	private int[] times;
-	private ArrayList<int[]> moves;
+	private static ArrayList<int[]> moves;
+	private static int[] goal;
+	private static int size = 4;
 	
 	public BridgeAndTorch(){
-		people = new int[5];
+		people = new int[size];
 	}
 	
-	public ArrayList<int[]> getPossibleMoves(){
-		return moves;
+	public static void getPossibleMoves(int[] current){
+		int[] temp = current;
+		moves = new ArrayList<int[]>();
+		if (temp[0] == 0){
+			temp[0] = 1;
+			while(!compareArrays(temp,goal)){
+				for(int i = temp.length-1;i>0;i--){
+					if(temp[i] == 0){
+						temp[i] = 1;
+						if(!compareArrays(temp,current) && !moves.contains(temp)){
+							moves.add(temp);
+						}
+						break;
+					}
+					temp[i]=0;
+				}
+				
+			}
+		}
+		
 	}
-	public void solve(){}
+	public static void generateGoal(){
+		goal = new int[size];
+		for (int i=0;i<size;i++){
+			goal[i] = 1;
+		}
+	}
+
+	public static void printArray(int[] array){
+		for (int i = 0; i<array.length;i++){
+			System.out.print(array[i] + " ");
+		}
+		System.out.println("\t");
+	}
+	public static boolean compareArrays(int[] arr1, int[] arr2){
+		if(arr1.length!=arr2.length) return false;
+		for(int i = 0; i < arr1.length; i++){
+			if(arr1[i]!=arr2[i]) return false;
+		}
+		return true;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BridgeAndTorch b = new BridgeAndTorch();
+		generateGoal();
+		
+		printArray(people);
+		System.out.println();
+		printArray(goal);
+		int[] temp = {1,1,1};
+		System.out.println(compareArrays(people,temp));
+		
+		getPossibleMoves(people);
+		
+		
 	}
-
+	
 }
