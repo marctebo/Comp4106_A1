21fc5b789ea8c14b1cddb35b7369374d3c102ca3
marctebo <marctebo@gmail.com>
2017-02-08 19:49:07 -0500

    Added a few skeleton methods

--- Comp4106_A1/src/BridgeAndTorch.java
+++ Comp4106_A1/src/BridgeAndTorch.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 
 public class BridgeAndTorch {
 
 	private int[] people;
 	private int[] times;
+	private ArrayList<int[]> moves;
+	
+	public BridgeAndTorch(){
+		people = new int[5];
+	}
+	
+	public ArrayList<int[]> getPossibleMoves(){
+		return moves;
+	}
+	public void solve(){}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
